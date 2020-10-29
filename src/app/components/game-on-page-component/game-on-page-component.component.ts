8ef4c0034097fa12772883d0b0d594ee99becef3
LukeGiles3 <luke.giles333@gmail.com>
2020-10-29 08:52:26 -0600

    addPlayer consolelog, all golf course data display

--- src/app/components/game-on-page-component/game-on-page-component.component.ts
+++ src/app/components/game-on-page-component/game-on-page-component.component.ts
@@ -1,6 +1,6 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Player } from './../../interfaces/player';
 import { ActivatedRoute } from '@angular/router';
-import { Courses } from './../../interfaces/course';
-import { Holes } from './../../interfaces/holes';
 import { Component, OnInit } from '@angular/core';
 import { HerokuCoursesAPIService } from './../../services/heroku-courses-api.service';
 
@@ -16,19 +16,28 @@ export class GameOnPageComponentComponent implements OnInit {
   image: any;
   frontNineYards = [];
   frontNinePar = [];
+  frontNineHoles = [];
   backNineYards = [];
   backNinePar = [];
+  backNineHoles = [];
   inYard: number;
   outYard: number;
   inPar: number;
   outPar: number;
   totalYard: number;
   totalPar: number;
+  player: Player;
+  form: FormGroup;
 
   constructor(
     private courseService: HerokuCoursesAPIService,
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    fb: FormBuilder
+    ) {
+      this.form = fb.group({
+        name: ['']
+     });
+    }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -40,7 +49,24 @@ export class GameOnPageComponentComponent implements OnInit {
 
       for (let i = 0; i <= 8; i++) {
         this.frontNineYards.push(this.holes[i].teeBoxes[0].yards);
+        this.frontNinePar.push(this.holes[i].teeBoxes[0].par);
+        this.frontNineHoles.push(this.holes[i].hole);
+        this.outYard = this.frontNineYards.reduce(function(a, b){ return a + b; }, 0);
+        this.outPar = this.frontNinePar.reduce(function(a, b){ return a + b; }, 0);
       }
+      for (let i = 9; i <= 17; i++) {
+        this.backNineYards.push(this.holes[i].teeBoxes[0].yards);
+        this.backNinePar.push(this.holes[i].teeBoxes[0].par);
+        this.backNineHoles.push(this.holes[i].hole);
+        this.inYard = this.backNineYards.reduce(function(a, b){ return a + b; }, 0);
+        this.inPar = this.backNinePar.reduce(function(a, b){ return a + b; }, 0);
+      }
+      this.totalYard = (this.outYard + this.inYard);
+      this.totalPar = (this.outPar + this.inPar);
     });
+  }
+
+  addPlayer(name: string): void {
+    console.log(name);
   }
 }
